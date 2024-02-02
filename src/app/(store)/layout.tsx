5c62c1ca354879ade9912e700c00da53cb846d35
jpedro002 <joaopsilvavolei@gmail.com>
2024-02-01 21:32:50 -0300

    feat: header of layout

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,15 @@
+import { Header } from '@/components/Header'
+import { ReactNode } from 'react'
+
+export default function DefaultStoreLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <div className="grid grid-rows-[5rem_1fr] bg-background max-w-[1440px] min-h-screen md:px-20 lg:px-40 mx-auto ">
+      <Header />
+      {children}
+    </div>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { MapPin, ShoppingCart } from 'lucide-react'
+
+export const Header = () => {
+  return (
+    <header className="flex justify-between items-center ">
+      <Link href={'/'}>
+        <Image alt="sfdsd" src="/Logo.png" width={70} height={30} />
+      </Link>
+      <div className="flex gap-4">
+        <div className="bg-purple-200 text-purplePesonalized-300 px-2 py-1.5 rounded-md flex items-center">
+          <MapPin className="w-5 h-w-5 " />
+          <h6 className="text-base">Porto Alegre, RS</h6>
+        </div>
+        <div
+          className="flex justify-between items-center
+         bg-yellowPesonalized-100 p-2 rounded-md text-yellowPesonalized-300"
+        >
+          <ShoppingCart className="w-5 h-w-5 " />
+        </div>
+      </div>
+    </header>
+  )
+}
